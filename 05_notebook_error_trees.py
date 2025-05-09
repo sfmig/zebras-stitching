f9aea4e45e1e3ec271e88f685cfff397c7c24e3b
niksirbi <niko.sirbiladze@gmail.com>
2025-05-09 19:33:50 +0100

    save trees with long trajectories that don't expeed a speed limit

--- 05_notebook_error_trees.py
+++ 05_notebook_error_trees.py
@@ -5,7 +5,10 @@ from itertools import groupby
 
 import matplotlib.pyplot as plt
 import numpy as np
-from movement.io import load_bboxes
+import xarray as xr
+from movement.io import load_bboxes, save_poses
+from movement.kinematics import compute_path_length, compute_speed
+
 
 # %matplotlib widget
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -20,89 +23,105 @@ ds = load_bboxes.from_via_tracks_file(
     file_path=filename, use_frame_numbers_from_file=False
 )
 
-# %%%%%%%%%%%%%%%%%%%%%%%%%%%%
-# Get length of trajectory (ie number of samples that are not nan)
-
-length_trajectory = np.array(
-    [
-        len(
-            list(
-                next(groupby(np.all(~np.isnan(ds.position.values), axis=1)[:, ind]))[1]
-            )
-        )
-        for ind in range(len(ds.individuals))
-    ]
+# %%
+# Visualise the tree trajectories
+ds.position.plot.line(
+    x="time",
+    row="space",
+    hue="individuals",
+    add_legend=False,
+    aspect=2,
+    size=2.5,
 )
 
-# %%%%%%%%%%%%%%%%%%%%%%%%%%%%
-# Total variation in x and y positions
 
-# Linearly interpolate position first?
+# %%
 
-total_variation_x = np.nansum(
-    np.abs(np.diff(ds.position.sel(space="x"), axis=0)), axis=0
+n_trees = len(ds.individuals)
+print(f"Number of total trees: {n_trees}")
+
+# %%
+# Measure number of valid points for each tree trajectory
+# (ie number of samples that are not nan)
+valid = ds.position.notnull().all(["space"]).sum(["time"])
+valid.plot.hist(bins=100);
+plt.title("Histogram of number of valid samples per tree")
+plt.xlabel("Number of valid samples")
+plt.ylabel("Count")
+plt.show()
+
+
+# keep only trees with > 300 valid points
+long_trees = ds.where(valid > 400, drop=True)
+print(f"Number of trees detected in >400 frames: {len(long_trees.individuals)}")
+
+long_trees.position.plot.line(
+    x="time",
+    row="space",
+    hue="individuals",
+    add_legend=False,
+    aspect=2,
+    size=2.5,
 )
-total_variation_y = np.nansum(
-    np.abs(np.diff(ds.position.sel(space="y"), axis=0)), axis=0
+plt.xlim(0, long_trees.time.size - 1)
+
+
+# %%
+# Construct a new poses dataset with only the long trees
+
+long_trees_ds = xr.Dataset(
+    {
+        "position": long_trees.position,
+        "confidence": long_trees.confidence,
+    }
+).expand_dims(
+    {"keypoints": ["centroid"]},
+    axis=-2,
 )
 
-# add and normalize by length of trajectory
-total_variation = (total_variation_x + total_variation_y) / length_trajectory
+print(long_trees_ds)
 
-# Remove values with 0 total variation
-total_variation[total_variation == 0] = np.nan
+# save to a new file
+save_poses.to_sleap_analysis_file(
+    long_trees_ds,
+    file_path="data/21Jan_007_tracked_trees_long.analysis.h5",
+)
 
+# %%
+# Drop trees with implausibly high speeds
+speed = compute_speed(long_trees.position)
 
-# %%%%%%%%%%%%%%%%%%%%%%%%%%%%
-# Plot total variation in x and y per individual
+speed.plot.line(
+    x="time",
+    hue="individuals",
+    add_legend=False,
+    aspect=3,
+    size=3,
+);
 
-fig, ax = plt.subplots()
+# %%
+speed_limit = 10  # pixels/frame
+# Drop trees whose max speed is > speed_limit
+long_slow_trees_ds = long_trees_ds.where(
+    (speed.max("time") < speed_limit), drop=True
+)
+print(f"Number of trees: {len(long_slow_trees_ds.individuals)}")
 
-ax.plot(total_variation_x, label="total variation in x-coordinate")
-ax.plot(total_variation_y, label="total variation in y-coordinate")
+long_slow_trees_ds.position.sel(keypoints="centroid").plot.line(
+    x="time",
+    row="space",
+    hue="individuals",
+    add_legend=False,
+    aspect=2,
+    size=2.5,
+)
+plt.xlim(0, long_slow_trees_ds.time.size - 1)
 
-ax.set_xlabel("tree")
-ax.set_ylabel("total variation (pixels)")
-ax.legend()
-
-
-fig, ax = plt.subplots()
-
-ax.plot(total_variation, label="total variation normalised by length of trajectory")   
-
-ax.set_xlabel("tree")
-ax.set_ylabel("total variation (pixels)")
-ax.legend()
-
-
-
-# %%%%%%%%%%%%%%%%%%%%%%%%%%%%
-# Sort by total variation
-# (We would like trajectories with LOW total variation)
-total_variation_no_nan = total_variation[~np.isnan(total_variation)]
-ind_sorted = np.argsort(total_variation_no_nan)
-
-
-# %%%%%%%%%%%%%%%%%%%%%%%%%%%%
-# Plot x,y positions
-
-fig, ax = plt.subplots()
-
-
-# plot first 10 trajectories (with MOST total variation)
-ax.plot(
-    ds.position.sel(space="x").isel(individuals=ind_sorted[:100]),
-    ds.position.sel(space="y").isel(individuals=ind_sorted[:100]),
-    label=ind_sorted[:100],
-)  
-ax.legend()
-
-fig, ax = plt.subplots()
-# plot last 10 trajectories (with MOST total variation)
-ax.plot(
-    ds.position.sel(space="x").isel(individuals=ind_sorted[-100:]),
-    ds.position.sel(space="y").isel(individuals=ind_sorted[-100:]),
-)  # ds.position.sel(space="y"), ".")
-
+# %%
+# Save the slow trees to a new file
+save_poses.to_sleap_analysis_file(
+    long_slow_trees_ds,
+    file_path="data/21Jan_007_tracked_trees_long-and-slow.analysis.h5",
+)
 
 # %%
